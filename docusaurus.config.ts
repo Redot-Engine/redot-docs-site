be08e144b20bae1f17f2d31f6d8dd90df695f987
MichaelFisher1997 <contact@michaelfisher.tech>
2026-02-16 01:49:46 +0000

    fix: correct editUrl branch from main to master

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -43,7 +43,7 @@ const config: Config = {
           sidebarPath: './sidebars.ts',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: 'https://github.com/redot-engine/redot-docs-site/tree/main/',
+          editUrl: 'https://github.com/redot-engine/redot-docs-site/tree/master/',
         },
         theme: {
           customCss: './src/css/custom.css',
